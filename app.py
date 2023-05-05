2cac15775777f5a32108e7452419738acbfa27b0
EmersonLima1 <74200172+EmersonLima1@users.noreply.github.com>
2023-05-05 12:59:24 -0300

    update app.py

--- app.py
+++ app.py
@@ -2740,6 +2740,17 @@ def main():
         arbitro_widget = st.selectbox('Árbitro:', options=arbitros)
         data_widget = st.date_input('Data da partida:')
 
+        opcoes = ['Padrão 1 - Confrontos diretos',
+                  'Padrão 2 - Histórico do campeonato',
+                  'Padrão 3 - Últimas 3 partidas em casa e últimas 3 partidas fora',
+                  'Padrão 4 - Últimas 5 partidas em casa e últimas 5 partidas fora',
+                  'Padrão 5 - Últimas 10 partidas em casa e últimas 10 partidas fora',
+                  'Padrão 6 - Últimas 3 partidas de modo geral',
+                  'Padrão 7 - Últimas 5 partidas de modo geral',
+                  'Padrão 8 - Últimas 10 partidas de modo geral']
+
+        padroes_selecionados = st.multiselect('Selecione os padrões de análise:', options=opcoes)
+
         # Adicionando botões de ação
         if st.button('Gerar previsões'):
             if time_fora_widget == time_casa_widget:
