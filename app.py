cbcd4c55f0742364d3db8578b7c513c4b3f0f14d
EmersonLima1 <74200172+EmersonLima1@users.noreply.github.com>
2023-05-08 11:51:47 -0300

    update app.py

--- app.py
+++ app.py
@@ -3024,8 +3024,7 @@ def main():
                         st.table(df_res)
                         st.table(df_inf)
                       else:
-                        df = padroes_usuario(time_casa_widget, time_fora_widget, arbitro_widget, multi_target_rfc, le, partidas_anteriores, acuracia, padroes_selecionados)
-                        st.dataframe(df)
+                        st.write('Cheguei até aqui')
 
                 except ValueError:
                     st.error("Data inválida. Por favor, selecione outra data.")
